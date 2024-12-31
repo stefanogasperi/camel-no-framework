7b0a1258d1c1078e679395c022f4192cf31e1e9e
N2-SGASPERI\s.gasperi <stefano.gasperi@gmail.com>
2024-12-31 10:10:03 +0100

    altre prove

--- src/main/java/com/gas/edu/examplesmain/RunFile2QueueRoute.java
+++ src/main/java/com/gas/edu/examplesmain/RunFile2QueueRoute.java
@@ -1,14 +1,15 @@
 package com.gas.edu.examplesmain;
 
 import com.gas.edu.routes.File2QueueRoute;
-import com.gas.edu.utils.Utility;
 import lombok.extern.java.Log;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
-import org.apache.camel.Component;
 import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
+
+import javax.jms.ConnectionFactory;
 
 
 @Log
@@ -19,17 +20,17 @@ public class RunFile2QueueRoute {
         System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "debug");
         ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
         String url="";
-        url="http://localhost:8161";
+        //url="http://localhost:8161";
         //url="localhost";
         //url="vm://localhost";
-        //url="tcp://localhost:61616";
-        activeMQConnectionFactory.setUserName("admin");
+        url="tcp://localhost:61616";
+        activeMQConnectionFactory.setUser("admin");
         activeMQConnectionFactory.setPassword("password");
         activeMQConnectionFactory.setBrokerURL(url);
 
-        //JmsComponent jmsComponent = new JmsComponent();
-        JmsComponent jmsComponent = JmsComponent.jmsComponentAutoAcknowledge(activeMQConnectionFactory);
-        //jmsComponent.setConnectionFactory(activeMQConnectionFactory);
+        JmsComponent jmsComponent = new JmsComponent();
+        //JmsComponent jmsComponent = JmsComponent.jmsComponentAutoAcknowledge(activeMQConnectionFactory);
+        jmsComponent.setConnectionFactory(activeMQConnectionFactory);
 
         try (CamelContext camelContext = new DefaultCamelContext()) {
 
--- src/main/java/com/gas/edu/routes/File2QueueRoute.java
+++ src/main/java/com/gas/edu/routes/File2QueueRoute.java
@@ -8,13 +8,14 @@ public class File2QueueRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("file:input-folder?antInclude=*.bat&noop=true")
-        //from("jms:queue:QueueAny1")
+        //from("file:input-folder?antInclude=*.bat&noop=true")
+        from("jms:queue:queue1")
                 .process(exchange -> {
                     Object obj = exchange.getMessage().getBody();
                     System.out.println(obj);
+                    exchange.getMessage().setBody(obj);
                 })
-                .convertBodyTo(String.class)
-                .to("jms:queue:QueueAny2");
+                //.convertBodyTo(String.class)
+                .to("jms:queue:queue-dest1");
     }
 }
